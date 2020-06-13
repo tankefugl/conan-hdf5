50171b39fb583ef63ce57b387d02da633be03f00
Joakim Haugen <joakim.haugen@gmail.com>
2020-06-13 17:09:21 +0200

    Add readme, CI gcc-6 and reorder matrix parameters

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,9 +1,12 @@
 #include <H5Cpp.h>
+#include <iostream>
 
 int main()
 {
-    hsize_t dimensions[] = {4, 6};  
+    hsize_t dimensions[] = {4, 6};
 	H5::H5File file("dataset.h5", H5F_ACC_TRUNC);
 	H5::DataSpace dataspace(2, dimensions);
 	H5::DataSet dataset = file.createDataSet("dataset", H5::PredType::STD_I32BE, dataspace);
+    std::cout << "High 5" << std::endl;
+    return EXIT_SUCCESS;
 }
