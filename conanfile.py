51d945fde3f2ab7cb0862a11bc68332593401cd0
Jarle Ladstein <jarle.ladstein@sintef.no>
2020-06-19 14:01:01 +0200

    Bugfix: do not remove option threadsafe if allow_unsupported is set

--- conanfile.py
+++ conanfile.py
@@ -57,7 +57,7 @@ class Hdf5Conan(ConanFile):
         if not self.options.enable_cxx:
             del self.settings.compiler.libcxx
             del self.settings.compiler.cppstd
-        if self.options.enable_cxx or self.options.hl or (self.settings.os == "Windows" and not self.options.shared):
+        if not self.options.allow_unsupported and (self.options.enable_cxx or self.options.hl or (self.settings.os == "Windows" and not self.options.shared)):
             del self.options.threadsafe
         if not bool(self.options.szip_support):
             del self.options.szip_encoding
